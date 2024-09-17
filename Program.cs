3c8114cfe67ffcc39759465efb7f9e166ac21f62
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-09-17 14:34:10 +0200

    Committing finnished methods

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
     static void Main(string[] args) {
         // 5 lottonummer;
 
-        static int getNumberOfTickets() {
+        static int GetNumberOfTickets() {
             bool isNumber;
             int number;
             
@@ -20,7 +20,7 @@ class Program
             return number;
         }
         
-        static int[] getTicketNumbers(int number, int[] array) {
+        static int[] GetTicketNumbers(int number, int[] array) {
             
             for (int i = 0; i < number; i++) {
                 bool isNumber;
@@ -37,47 +37,58 @@ class Program
             return array;
         }
         
-        // Initialises arrays.
-        int numberOfTickets = getNumberOfTickets();
+        static int[] GetWinningNumbers(int[] array) {
+            Random random = new Random();
+            for (int i = 0; i < 3; i++) {
+                int number;
+            
+                do {
+                    number = random.Next(1, 51);
+                } while (array.Contains(number));
+
+                array[i] = number;
+            }
+
+            return array;
+        }
+        
+        static int ComapareNumbers(int[] playerArray, int[] randomArray) {
+            int matchingNumbers = 0;
+            foreach (int number in randomArray) {
+                if (playerArray.Contains(number)) matchingNumbers++;
+            }
+
+            return matchingNumbers;
+        }
+        
+        void ShowResult(int number) {
+            switch (number) {
+                case 1 : 
+                    Console.WriteLine("You got one number right!");
+                    break;
+                case 2 : 
+                    Console.WriteLine("You got two numbers right!");
+                    break;
+                case 3 :
+                    Console.WriteLine("You won the JackPot!!! Congratulations!");
+                    break;
+                default :
+                    Console.WriteLine("You won nothing, better luck next time!");
+                    break;
+            }
+        }
+        
+        int numberOfTickets = GetNumberOfTickets();
         
         int[] tickets = new int[numberOfTickets];
         int[] winningNumbers = new int[3];
-        
-        
-        
-        
-        
-        // Get winning numbers.
-        for (int i = 0; i < 3; i++) {
-            int number;
-            
-            do {
-                number = random.Next(1, 51);
-            } while (winningNumbers.Contains(number));
 
-            winningNumbers[i] = number;
-        }
+        tickets = GetTicketNumbers(numberOfTickets, tickets);
+        winningNumbers = GetWinningNumbers(winningNumbers);
+
+        int matchingNumbers = ComapareNumbers(tickets, winningNumbers);
+
+        ShowResult(matchingNumbers);
         
-        
-        // Validates matches between chosen numbers and winning numbers.
-        int matchingNumbers = 0;
-        foreach (int number in winningNumbers) {
-            if (tickets.Contains(number)) matchingNumbers++;
-        }
-        
-        switch (matchingNumbers) {
-            case 1 : 
-                Console.WriteLine("You got one number right!");
-                break;
-            case 2 : 
-                Console.WriteLine("You got two numbers right!");
-                break;
-            case 3 :
-                Console.WriteLine("You won the JackPot!!! Congratulations!");
-                break;
-            default :
-                Console.WriteLine("You won nothing, better luck next time!");
-                break;
-        }
     }
 }
